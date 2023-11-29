98cc7c7819770291575992de436c52d4444da9e7
Nate Wagner <nate.o.wagner@gmail.com>
2023-11-29 08:27:22 -0500

    fix: remove header on buzzer page

--- src/Router.tsx
+++ src/Router.tsx
@@ -13,8 +13,16 @@ import BuzzerPage from "./components/BuzzerPage";
 import { RealtimeStatus } from "./buzzers/ably-realtime";
 import BuzzerBox from "./components/BuzzerBox";
 
-const loadRealtime = () => {
+const loadRealtime = async () => {
   RealtimeStatus.connect();
+  return checkAuth();
+};
+
+const checkAuth = async () => {
+  const heartbeat = await (await fetch("/api/heartbeat")).json();
+  if (!heartbeat.alive) {
+    return redirect("/login");
+  }
   return null;
 };
 
@@ -22,31 +30,25 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    loader: async () => {
-      const heartbeat = await (await fetch("/api/heartbeat")).json();
-      if (!heartbeat.alive) {
-        return redirect("/login");
-      }
-      return null;
-    },
+    loader: checkAuth,
     errorElement: <Error404 />,
     children: [
       { path: "", element: <QuizPage /> },
       { path: "set", element: <SetPage /> },
       { path: "math", element: <MathPage /> },
-      {
-        path: "buzzer",
-        element: <BuzzerPage />,
-        loader: loadRealtime,
-      },
-      {
-        path: "buzzer-box",
-        element: <BuzzerBox />,
-        loader: loadRealtime,
-      },
     ],
   },
   { path: "/login", element: <Login /> },
+  {
+    path: "/buzzer",
+    element: <BuzzerPage />,
+    loader: loadRealtime,
+  },
+  {
+    path: "/buzzer-box",
+    element: <BuzzerBox />,
+    loader: loadRealtime,
+  },
 ]);
 
 export default function Router() {
