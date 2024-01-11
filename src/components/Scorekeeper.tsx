9d61abac9761c661068b9be750dbf6051f17853e
Nate Wagner <nate.o.wagner@gmail.com>
2024-01-11 14:14:09 -0500

    feat: add new score controls to scorekeeper page

--- src/components/Scorekeeper.tsx
+++ src/components/Scorekeeper.tsx
@@ -14,7 +14,32 @@ export default function Scorekeeper() {
       team,
     });
   };
+  const tieBreaker = Math.abs(
+    (teamScores[0]?.score ?? 0) - (teamScores[1]?.score ?? 0)
+  );
   const [customScoreInputs, setCustomScoreInputs] = useState({ a: 0, b: 0 });
+
+  function createScoreButton(
+    score: number,
+    team: "a" | "b",
+    key?: string | number
+  ) {
+    if (score === 0) {
+      return null;
+    }
+    return (
+      <button
+        key={key}
+        onClick={() => {
+          addPoints(score, team);
+        }}
+        className="p-2 px-4 bg-gray-400 active:bg-gray-300 rounded-md"
+      >
+        {score > 0 ? "+" : score < 0 ? "-" : ""}
+        {Math.abs(score)}
+      </button>
+    );
+  }
   if (!isHostConnected) {
     return (
       <div
@@ -47,10 +72,11 @@ export default function Scorekeeper() {
               "flex-col",
               "rounded-md",
               "gap-2",
+              "items-center",
               getTeamColors(team)
             )}
           >
-            <span className="text-2xl text-center">
+            <span className="text-2xl">
               Team {team.toUpperCase()}:{" "}
               <span className="text-red-400">
                 {teamScores.find((teamScore) => teamScore.team === team)
@@ -58,22 +84,12 @@ export default function Scorekeeper() {
               </span>
             </span>
             <div className="flex gap-2">
-              {[1, 2, /*...(tieBreaker > 2 ? [tieBreaker] : []),*/ -1].map(
-                (increment, index) => (
-                  <button
-                    key={index}
-                    onClick={() => {
-                      addPoints(increment, team);
-                    }}
-                    className="p-2 px-4 bg-gray-400 active:bg-gray-300 rounded-md"
-                  >
-                    {increment > 0 ? "+" : ""}
-                    {increment}
-                  </button>
-                )
+              {[1, 2, -1].map((increment, index) =>
+                createScoreButton(increment, team, index)
               )}
             </div>
-            <div className="flex gap-2 mx-auto">
+            <span>Custom Input</span>
+            <div className="flex gap-2">
               <ExpandingInput
                 type="text"
                 className="py-1 p-3 rounded-md bg-white"
@@ -84,34 +100,37 @@ export default function Scorekeeper() {
                     : customScoreInputs[team]
                 }
                 onChange={(e) => {
+                  const filteredValue = e.target.value.replaceAll(
+                    /\s|[-\.]|(?:^[^0-9])/g,
+                    ""
+                  );
                   try {
-                    const newValue = parseInt(e.target.value);
+                    const newValue = parseInt(filteredValue);
                     setCustomScoreInputs((old) => {
                       return {
                         ...old,
-                        [team]: newValue,
+                        [team]: Math.abs(newValue),
                       };
                     });
                   } catch (e) {}
                 }}
               />
-              <button
-                onClick={() => {
-                  addPoints(
-                    Number.isNaN(customScoreInputs[team])
-                      ? 0
-                      : customScoreInputs[team],
-                    team
-                  );
-                }}
-                className="p-2 px-4 bg-gray-400 active:bg-gray-300 rounded-md"
-              >
-                Add{" "}
-                {Number.isNaN(customScoreInputs[team])
-                  ? 0
-                  : customScoreInputs[team]}
-              </button>
+              {[true, false].map((isAdding) =>
+                createScoreButton(
+                  Number.isNaN(customScoreInputs[team])
+                    ? 0
+                    : (isAdding ? 1 : -1) * customScoreInputs[team],
+                  team,
+                  isAdding ? "add" : "sub"
+                )
+              )}
             </div>
+            {tieBreaker > 2 && (
+              <>
+                <span>Tie Breaker</span>
+                {createScoreButton(tieBreaker, team)}
+              </>
+            )}
           </div>
         ))}
       </div>
