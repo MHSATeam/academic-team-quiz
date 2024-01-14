928c9efae4ad57c0d6c4e8bc4fbc4d39a14aca65
Nate Wagner <nate.o.wagner@gmail.com>
2024-01-14 16:57:28 -0500

    fix: remove distDir setting from next js config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  distDir: "./dist",
   webpack: (config) => {
     if (!("ignoreWarnings" in config)) {
       config.ignoreWarnings = [];
