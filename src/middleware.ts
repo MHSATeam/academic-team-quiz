daebc03793e15b14945fe8d8fb5bd4d1519ff012
Nate Wagner <nate.o.wagner@gmail.com>
2024-01-15 17:32:39 -0500

    fix: remove logo from auth requirement

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,7 @@
 import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
 
+export const config = {
+  matcher: "/((?!mustangs-amazons-square-logo.png).*)",
+};
+
 export default withMiddlewareAuthRequired();
