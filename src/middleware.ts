09fe404c8f354d8cdabafb626e51cb52b4f433b1
Nate Wagner <nate.o.wagner@gmail.com>
2024-01-15 21:34:17 -0500

    fix: remove auth on ably and buzzer log routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
 
 export const config = {
-  matcher: "/((?!mustangs-amazons-square-logo.png|buzzer|scorekeeper).*)",
+  matcher:
+    "/((?!mustangs-amazons-square-logo.png|buzzer|scorekeeper|api/ably-auth|api/log-buzz).*)",
 };
 
 export default withMiddlewareAuthRequired();
