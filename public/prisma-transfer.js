bc29bfb24738847f275951dcdeb10ad37c6caf50
Nate Wagner <nate.o.wagner@gmail.com>
2024-01-23 22:58:42 -0500

    feat: update prisma transfer script to utilize bulk insert

--- public/prisma-transfer.js
+++ public/prisma-transfer.js
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import * as fs from "fs";
 import path from "path";
+import { exit } from "process";
 
 const prisma = new PrismaClient();
 
@@ -13,25 +14,21 @@ const RoundID = 10; // Luke Knull's Quizlets: (on local)
 const set = JSON.parse(
   fs.readFileSync(path.join(process.cwd(), pathToSet), "utf-8")
 );
-const newQuestions = [];
-for (const question of set) {
+if (set.length === 0) {
+  console.log("No questions found; aborting...");
+} else {
   try {
-    newQuestions.push(
-      await prisma.question.create({
-        data: {
-          question: question.definition,
-          answer: question.term,
-          createdYear: 2015,
-          categoryId: CategoryID,
-          roundId: RoundID,
-        },
-      })
-    );
+    const questions = await prisma.question.createMany({
+      data: set.map((question) => ({
+        question: question.definition,
+        answer: question.term,
+        createdYear: 2015,
+        categoryId: CategoryID,
+        roundId: RoundID,
+      })),
+    });
+    console.log(`Successfully uploaded ${questions.count} questions`);
   } catch (e) {
     console.error(e);
-    console.log(newQuestions.slice(newQuestions.length - 4));
-    break;
   }
 }
-
-console.log(`Successfully uploaded ${newQuestions.length} questions`);
